52b5ddc5a6e08929177493284c57b2a88400467b
YInusa Amusa <69213231+ZEscanor@users.noreply.github.com>
2023-02-19 05:12:21 -0600

    Update HeroBanner.jsx

--- e-commerce-web/components/HeroBanner.jsx
+++ e-commerce-web/components/HeroBanner.jsx
@@ -19,7 +19,7 @@ const HeroBanner = ({heroBanner}) => {
         <button type="button">{heroBanner.buttonText} </button>
       </Link>
       <div className='desc'>
-        <h5> Desription</h5>
+        <h5> Description</h5>
         <p> {heroBanner.desc}</p>
       </div>
      </div>
