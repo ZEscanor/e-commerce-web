ec57106ce1c54b2b74eb1e98508c64f959111d20
Yinusa <yinusaamusa137@gmail.com>
2023-04-11 03:41:17 -0500

    added sort and mission statement

--- e-commerce-web/pages/about/index.js
+++ e-commerce-web/pages/about/index.js
@@ -2,11 +2,23 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div style={{fontWeight:800, fontSize:25}}>About US 
+    <div style={{fontWeight:800, fontSize:20}}>About US 
          <p style={{color: "red", fontWeight: 400}}>
           This world is filled with sounds, Some may be pleasant and some not so much. What is we could control the sound we hear and filter out that bad sound.
           In todays world that is very much possible with new Noise-Cancelling Headphones. Headphone Mart Was Created to deliver the best "sounding" products to you.
           Our products have been imported all across the seven seas and centralized in one place for your convenience .
+
+Welcome to Headphone Mart! At Headphone Mart, we are passionate about providing the ultimate audio experience. As a specialized ecommerce website, we offer a wide selection of high-quality headphones to cater to all music lovers, gamers, professionals, and audio enthusiasts. <br/><br/>
+
+Our carefully curated collection features the latest and most innovative headphone models from renowned brands in the industry. From over-ear to on-ear, in-ear to wireless, noise-cancelling to gaming headsets, we have something for everyone. We understand that headphones are not just accessories, but a gateway to immerse yourself in the world of music, gaming, and entertainment.<br/><br/>
+
+At Headphone Mart, we are committed to offering superior customer service. Our team of headphone experts is always ready to assist you in finding the perfect pair of headphones that meet your specific needs and preferences. Whether you are a bass head, an audiophile, a sports enthusiast, or a traveler, we have the right headphones for you.<br/><br/>
+
+We take pride in providing a seamless and secure online shopping experience. Our website is user-friendly, with detailed product descriptions, images, and customer reviews to help you make an informed decision. We also offer fast and reliable shipping, hassle-free returns, and excellent after-sales support to ensure your satisfaction.<br/><br/>
+
+Headphone Mart is more than just an ecommerce website; it's a community for headphone enthusiasts. We strive to foster a sense of belonging by sharing our knowledge, tips, and recommendations through our blog and social media channels. We also offer exclusive promotions, discounts, and loyalty rewards to our valued customers.<br/><br/>
+
+Thank you for choosing Headphone Mart as your destination for premium headphones. Join us on this audio journey and elevate your listening experience to new heights!
          </p>
 
 
